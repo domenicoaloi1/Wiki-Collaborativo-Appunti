60b6421f53bbb64a29b5d258fbed03ce61349d89
domenicoaloi1 <domenico.aloi1@studenti.unipr.it>
2026-02-04 22:31:09 +0100

    feat: creo una filter strategy strategy che usa IN e accetta una lista

---
+++ project/backend/Model/Strategy/IdInFilter.php
@@ -0,0 +1,17 @@
+<?php
+// backend/Model/Strategy/IdInFilter.php
+
+class IdInFilter implements FilterStrategy {
+    private $ids;
+
+    public function __construct(array $ids) {
+        $this->ids = array_map('intval', $ids);
+    }
+
+    public function buildCriteria(QueryObject $query): void {
+        if (!empty($this->ids)) {
+            $valueList = implode(',', $this->ids);
+            $query->addCriteria(new Criteria('id', 'IN', "($valueList)"));
+        }
+    }
+}
